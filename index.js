afa397edf0c00fc6b80904a1e2616def596e7efc
Carlos Ramirez <carlosramirez686@gmail.com>
2021-12-20 19:07:26 -0800

    assignment completed

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-const employee = Object; {
-    name:[]
-    streetAddress:[]
+const employee = {
+    name:"Sam",
+    streetAddress:"11 Broadway"
 }
 
 function updateEmployeeWithKeyAndValue (employee, key, value){
-    return {...employee, [key]: value };
+    return {...employee, ...{[key]: value} };
 }
+function destructivelyUpdateEmployeeWithKeyAndValue (employee, key, value){
+    employee[key] = value;
+    return employee
+}
+    
+function deleteFromEmployeeByKey (employee, key, value){
+    const newObj = {...employee};
+    newObj[key] = value;
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey (employee, key, value){
+    employee[key] = value;
+    return employee
+}
+
